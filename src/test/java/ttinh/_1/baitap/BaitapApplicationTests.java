7df22fd709c078185b63688456b30e8f04f0340a
cocdeptraihaha <firefrog2804@gmail.com>
2024-11-26 13:46:06 +0700

    a

---
+++ src/test/java/ttinh/_1/baitap/BaitapApplicationTests.java
@@ -0,0 +1,13 @@
+package ttinh._1.baitap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
